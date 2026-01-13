ba77ba272ac0679f07a170acdbf0c9cfa0e3b3fe
samuelblanck92-sys <samuelblanck92@gmail.com>
2026-01-13 02:32:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 alert("App carregado com sucesso");
+eConfig = {
+  apiKey: "AIzaSyAR8XA0xUSgA731KPjLEO81ziNiDhvNyp0",
+  authDomain: "nosso-calendario-ac80e.firebaseapp.com",
+  projectId: "nosso-calendario-ac80e",
+  storageBucket: "nosso-calendario-ac80e.firebasestorage.app",
+  messagingSenderId: "218159755152",
+  appId: "1:218159755152:web:c1c9493c42cd5e31f3cd15"
+};
